de480e41783686eb58a6c2e90ca1a09d5d566aa4
SerejKa-lab <seruogacom@mail.ru>
2020-06-30 17:41:13 +0300

    Updates

---
+++ precache-manifest.0c7929921a905ec61ea5636f4bd41ef2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e9ca0857357d47b0940c4689405bc0",
+    "url": "/starwars-db/index.html"
+  },
+  {
+    "revision": "992d7d7515cf54d40918",
+    "url": "/starwars-db/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d722949f779c16bacc75",
+    "url": "/starwars-db/static/css/main.50ae3836.chunk.css"
+  },
+  {
+    "revision": "992d7d7515cf54d40918",
+    "url": "/starwars-db/static/js/2.103952fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-db/static/js/2.103952fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d722949f779c16bacc75",
+    "url": "/starwars-db/static/js/main.d5fd8bd8.chunk.js"
+  },
+  {
+    "revision": "9458d5667870c73606c0",
+    "url": "/starwars-db/static/js/runtime-main.64c60e39.js"
+  },
+  {
+    "revision": "e749583cfa11086293efbd3a3db86e61",
+    "url": "/starwars-db/static/media/Preloader.e749583c.svg"
+  },
+  {
+    "revision": "669ab4de3ffc05c4da4b9640b95575d0",
+    "url": "/starwars-db/static/media/error.669ab4de.png"
+  }
+]);
